0cde57eb65b2f946710d5518e06c7cc21589dd95
PrabodhaJayasinghe <110682835+PrabodhaJayasinghe@users.noreply.github.com>
2024-08-28 06:47:13 +0530

    Final

--- spotify-clone/src/components/DisplayAlbum.jsx
+++ spotify-clone/src/components/DisplayAlbum.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
 import { albumsData, assets, songsData } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
   const albumData = albumsData[id];
   console.log(albumData);
+  const {playWithId} = useContext(PlayerContext)
 
   return (
     <>
       <Navbar />
-      <div className = 'mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
+      <div className = 'flex flex-col gap-8 mt-10 md:flex-row md:items-end'>
         <img className="w-48 rounded" src={albumData.image} alt=""/>
         <div className="flex flex-col">
             <p>Playlist</p>
-            <h2 className="text-5xl font-bold mb-4 md:text-7xloo">{albumData.name}</h2>
+            <h2 className="mb-4 text-5xl font-bold md:text-7xloo">{albumData.name}</h2>
             <h4>{albumData.Desc}</h4>
             <p className="mt-1">
                 <img className="inline-block w-5" src={assets.spotify_logo} alt=""/>
@@ -32,12 +34,12 @@ const DisplayAlbum = () => {
         <p><b className="mr-4">#</b>Title</p>
         <p>Album</p>
         <p className="hidden sm:block">Date Added</p>
-        <img className="m-auto w-4" src={assets.clock_icon} alt=""/>
+        <img className="w-4 m-auto" src={assets.clock_icon} alt=""/>
         </div>
         <hr/>
         {
         songsData.map((item,index) => (
-            <div key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"> 
+            <div onClick={() => playWithId(item.id)} key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"> 
                 <p className="text-white">
                     <b className="mr-4 text-[#a7a7a7">
                         {index+1}
